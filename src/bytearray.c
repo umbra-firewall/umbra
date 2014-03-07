2cfc767be18de5587d499f4d576440480ea14b00
Travis Finkenauer <tmfink@umich.edu>
2014-03-07 01:16:32 -0500

    Implemented simple bytearray

---
+++ src/bytearray.c
@@ -0,0 +1,65 @@
+#include "bytearray.h"
+
+bytearray_t* new_bytearray() {
+    bytearray_t *ba = calloc(1, sizeof(bytearray_t));
+    if (ba  == NULL) {
+        perror("calloc");
+        abort();
+    }
+
+    ba->data = calloc(1, DEFAULT_BYTEARRAY_ALLOC);
+    if (ba->data == NULL) {
+        free(ba);
+        perror("calloc");
+        abort();
+    }
+    ba->len = 0;
+    ba->_alloc_len = DEFAULT_BYTEARRAY_ALLOC;
+
+    return ba;
+}
+
+void bytearray_free(bytearray_t *ba) {
+    if (ba == NULL) {
+        fprintf(stderr, "Tried to free null bytearray\n");
+        abort();
+    }
+
+    free(ba->data);
+    free(ba);
+}
+
+void bytearray_append(bytearray_t *ba, uint8_t *data, size_t len) {
+    if (ba == NULL) {
+        fprintf(stderr, "Tried to append null bytearray\n");
+        abort();
+    }
+
+    size_t new_len = ba->len + len;
+    if (new_len > ba->_alloc_len) {
+        /* Realloc more space */
+        ba->_alloc_len = new_len;
+        ba->data = realloc(ba->data, ba->_alloc_len);
+        if (ba->data == NULL) {
+            perror("realloc");
+            abort();
+        }
+    }
+
+    memcpy(ba->data + ba->len, data, len);
+}
+
+void bytearray_truncate_front(bytearray_t *ba, size_t trunc_amt) {
+    if (ba == NULL) {
+        fprintf(stderr, "Tried to truncate null bytearray\n");
+        abort();
+    }
+    if (trunc_amt > ba->len) {
+        fprintf(stderr, "Tried to truncate bytearray by more than size\n");
+        abort();
+    }
+
+    size_t new_len = ba->len - trunc_amt;
+    memmove(ba->data, ba->data + trunc_amt, new_len);
+    ba->len = new_len;
+}
---
+++ src/bytearray.h
@@ -0,0 +1,23 @@
+#ifndef BYTEARRAY_H
+#define BYTEARRAY_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define DEFAULT_BYTEARRAY_ALLOC 2048
+
+typedef struct {
+    uint8_t *data;
+    size_t len;
+    size_t _alloc_len;
+} bytearray_t;
+
+bytearray_t* new_bytearray();
+void bytearray_free(bytearray_t *ba);
+void bytearray_append(bytearray_t *ba, uint8_t *data, size_t len);
+void bytearray_truncate_front(bytearray_t *ba, size_t trunc_amt);
+
+#endif
